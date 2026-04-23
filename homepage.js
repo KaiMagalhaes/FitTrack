b5744af90680db1102cbe70b22e145efcd15d404
K4ir0z <kai.magalhaes.324268777@msft.cesae.pt>
2026-04-23 16:45:57 +0100

    Cores alteração

--- homepage.js
+++ homepage.js
@@ -1,2 +1,9 @@
 
 // ---------------------------------------BOTAO TEMA--------------------------------------
+const btnTema = document.getElementById("btnTema");
+
+btnTema.onclick = () => {
+  const elemento = document.documentElement;
+  const novoTema = elemento.getAttribute("data-bs-theme") === "dark" ? "light" : "dark";
+  elemento.setAttribute("data-bs-theme", novoTema);
+};
