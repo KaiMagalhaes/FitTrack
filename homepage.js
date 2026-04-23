7db7889fd04f7f13b9a4bfce29a5fdd7b9218f7c
K4ir0z <kai.magalhaes.324268777@msft.cesae.pt>
2026-04-23 16:11:41 +0100

    CSS e Background

--- homepage.js
+++ homepage.js
@@ -4,6 +4,6 @@ const btnTema = document.getElementById("btnTema");
 
 btnTema.onclick = () => {
   const elemento = document.documentElement;
-  const nvoT = elemento.getAttribute("data-bs-theme") === "dark" ? "light" : "dark";
+  const novoTema = elemento.getAttribute("data-bs-theme") === "dark" ? "light" : "dark";
   elemento.setAttribute("data-bs-theme", novoTema);
 };
