a50b1a58b651f2f578ffe2d006a1959aa62a333b
K4ir0z <kai.magalhaes.324268777@msft.cesae.pt>
2026-04-23 16:36:45 +0100

    Main

--- homepage.js
+++ homepage.js
@@ -1,9 +1,2 @@
 
 // ---------------------------------------BOTAO TEMA--------------------------------------
-const btnTema = document.getElementById("btnTema");
-
-btnTema.onclick = () => {
-  const elemento = document.documentElement;
-  const novoTema = elemento.getAttribute("data-bs-theme") === "dark" ? "light" : "dark";
-  elemento.setAttribute("data-bs-theme", novoTema);
-};
